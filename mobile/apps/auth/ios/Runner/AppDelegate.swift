0fc4185af98cdc8c3abdeceba86f429bb973968b
grittypuffy <grittypuffy@riseup.net>
2026-09-21 10:15:13 +0530

    [mob][auth] Remove redundant method and dead code in AppDelegate and update Info.plist
    Remove redundant application override and URL handling in AppDelegate and update UISceneClassName in Info.plist

--- mobile/apps/auth/ios/Runner/AppDelegate.swift
+++ mobile/apps/auth/ios/Runner/AppDelegate.swift
@@ -9,24 +9,10 @@ import app_links
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    if let url = AppLinks.shared.getLink(launchOptions: launchOptions) {
-      AppLinks.shared.handleLink(url: url)
-    }
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
-  }
-
-  override func application(
-    _ application: UIApplication,
-    configurationForConnecting connectingSceneSession: UISceneSession,
-    options: UIScene.ConnectionOptions
-  ) -> UISceneConfiguration {
-    let configuration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
-    configuration.delegateClass = FlutterSceneDelegate.self
-    return configuration
   }
 }
